806f1315cfcf831008012c7c96c635bb11d08ff5
juanpab53 <juan.pablo.aparicio1.rojas@gmail.com>
2025-11-05 17:46:09 -0500

    Cambios en la presentación de comentarios en el codigo

--- ExpresionesRegulares/src/main/java/com/regex/regexevaluator/model/RegexValidator.java
+++ ExpresionesRegulares/src/main/java/com/regex/regexevaluator/model/RegexValidator.java
@@ -13,11 +13,6 @@ public class RegexValidator {
      * @return true si la cadena es una placa válida, false en caso contrario.
      */
     public boolean esPlacaDeCarroValida(String placa) {
-        // La expresión regular ^[A-Z]{3}\d{3}$ valida:
-        // ^ - Inicio de la cadena.
-        // [A-Z]{3} - Exactamente tres letras mayúsculas.
-        // \d{3} - Exactamente tres dígitos (equivalente a [0-9]{3}).
-        // $ - Fin de la cadena.
         String expresion = "^[A-Z]{3}\\d{3}$";
         return Pattern.matches(expresion, placa);
     }
@@ -30,11 +25,6 @@ public class RegexValidator {
      * @return true si la cadena es un codigo postal válido para Medellín, false en caso contrario.
      */
     public boolean esCodPostalValido(String codPostal){
-        // La expresión regular ^05\d{4}$ valida:
-        // ^ - Inicio de la cadena
-        // 05 - Los caracteres iniciales que ha de tener 
-        // \d{4} - Exactamente 4 digitos
-        // $ - Fin de la cadena
         String expresion = "^05\\d{4}$";
         return Pattern.matches(codPostal, expresion);
     }
@@ -47,11 +37,6 @@ public class RegexValidator {
      * @return true si la cadena es un número fijo valido en Medellín, false en caso contrario.
     */ 
     public boolean esNumFijoValido(String numFijo){
-        //La expresión regular ^604\\d{7} valida:
-        // ^ - Inicio de la cadena
-        // 604 - Los caracteres iniciales que ha de tener 
-        // \d{7} - Exactamente 7 digitos
-        // $ - Fin de la cadena
         String expresion = "^604\\d{7}$";
         return Pattern.matches(numFijo, expresion);
     }
@@ -64,11 +49,6 @@ public class RegexValidator {
      * @return true si la cadena es un número de celular valido en Colombia, false en caso contrario.
     */
     public boolean esNumCelularValido(String numCelular){
-        // La expresión regular ^3\d{9}$ valida:
-        // ^ - Inicio de la cadena
-        // 3 - El caracter inicial
-        // \d{9} - Exactamente 9 digitos
-        // $ - Fin de la cadena
         String expresion = "^3\\d{9}$";
         return Pattern.matches(numCelular, expresion);
     }
@@ -81,14 +61,9 @@ public class RegexValidator {
      * @return true si la cadena es un correo electronico valido, false en caso contrario.
     */
     public boolean esEmailValido(String email){
-        // La expresión regular ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ valida:
-        // ^ - Inicio de la cadena
         // [a-zA-Z0-9._%+-]+ - Es la parte de example: uno o mas caracteres de letras, números o ._%+-
-        // @ - El caracter @
         // [a-zA-Z0-9.-]+ - Es la parte de domain: uno o más caracteres de letras, números, punto o guion.
-        // \. - El caracter .
         // [a-zA-Z]{2,} - Al menos dos caracteres de letras
-        // $ - Fin de la cadena
         String expresion = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         return Pattern.matches(email, expresion);
     }
@@ -101,15 +76,7 @@ public class RegexValidator {
      * @return true si la cadena tiene el formato de fecha valido, false en caso contrario. 
     */
     public boolean esFechaValida(String fecha){
-        // La expresión regular ^(0[1-9]|[12]\\d|3[01])\\/(0[1-9]|1[0-2])\\/\\d{4}$ valida:
-        // ^ - Inicio de la cadena.
-        // (0[1-9]|[12]\\d|3[01]) - Día : 01-09, 10-29, 30-31.
-        // \/ - Separador '/'.
-        // (0[1-9]|1[0-2]) - Mes : 01-09, 10-12.
-        // \/ - Separador '/'.
-        // \d{4} - Año : 4 dígitos.
-        // $ - Fin de la cadena.
-        // Nota: El metódo valida los rangos numéricos, pero no valida la lógica del calendarip.
+        // El metódo valida los rangos numéricos, pero no valida la lógica del calendarip.
         String expresion = "^(0[1-9]|[12]\\d|3[01])\\/(0[1-9]|1[0-2])\\/\\d{4}$";
         return Pattern.matches(expresion, fecha);
     }
